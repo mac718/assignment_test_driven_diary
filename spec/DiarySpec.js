8fd2f303d88c0ccd545ac96b1ee2bcb65cd193ec
mac718 <mac718@gmail.com>
2018-11-28 09:59:13 -0500

    Add tests for .entry, .entries., .tags, and .entryWithTag

--- spec/DiarySpec.js
+++ spec/DiarySpec.js
@@ -1,13 +1,31 @@
+const diary = require('../lib/diary');
 describe('Diary', () => {
+  
+  describe('.entries', () => {
+    it('lists the entries in the diary', () => {
+      expect(diary.entries().length).toEqual(0);
+    })
+  })
+
   describe('.entry', () => {
-    it('adds a new entry to the diary', () => {
-      class Diary {
-        entry() {};
-        entries() {};
-      };
-      let diary = new Diary();
-      let entry = diary.entry('Hello');
-      expect(diary.entries()).toContain(entry);
+    it('Adds and entry to the journal', () => {
+      diary.entry('Hey');
+      expect(diary.entries().length).toEqual(1);
+    })
+  })
+
+  describe('.tags', () => {
+    it('Lists the tags contained in all entries', () => {
+      expect(diary.tags()[0]).toEqual(null);
+      diary.entry("Sup brah! #brahsforever");
+      expect(diary.tags()[0]).toEqual('brahsforever');
+    })
+  })
+
+  describe('.entriesWithTag', () => {
+    it('Returns a list of entries with the given tag', () => {
+      diary.entry("Sup brah! #brahsforever");
+      expect(diary.entriesWithTag('brahsforever')).toContain("Sup brah! #brahsforever");
     })
   })
 })
